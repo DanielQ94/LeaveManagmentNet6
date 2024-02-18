e05378e763ab0f61b015eb194af552f51f0758ce
DanielQ94 <daniel.quezada18@gmail.com>
2024-02-17 21:55:17 -0500

    Added Auto Mapper

---
+++ LeaveManagment.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagment.Web.Data;
+using LeaveManagment.Web.Models;
+
+namespace LeaveManagment.Web.Configurations
+{
+    public class MapperConfig: Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagment.Web/Models/LeaveTypeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagment.Web.Models
+{
+    public class LeaveTypeViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        public string? Name { get; set; }
+
+        [Display(Name="Default Number Of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
